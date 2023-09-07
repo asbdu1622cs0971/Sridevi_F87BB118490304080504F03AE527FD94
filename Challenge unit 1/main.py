2ec6b173abf43157ae3d0e5f9eef6a7c442a8623
asbdu1622cs0971 <144306419+asbdu1622cs0971@users.noreply.github.com>
2023-09-07 11:32:36 +0530

    Add files via upload

---
+++ Challenge unit 1/main.py
@@ -0,0 +1,11 @@
+def isleepyear(year)
+if (year℅4==0 and year℅100!)or year℅400==0:                  
+  return true                   
+else:
+  return false                 
+year=int(input ("enter a year! "))
+if is leep year (year):    
+  print("{}is a leep year",format(year))
+else:
+  print(" {}is not a leep year",format(year))
+  )
